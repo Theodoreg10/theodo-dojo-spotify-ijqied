9e04717431caa5437a39b92ee429192c9c6a1482
Theodoreg10 <theodoregnimavo6@gmail.com>
2023-11-16 18:51:09 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
-
-const [trackIndex, setTrackIndex] = useState(0)
-
-const goToNextTrack = () => {
-  setTrackIndex(trackIndex + 1);
-}
+import { useQuery } from 'react-query';
+import {fetchTracks} from './lib/fetchTracks';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -15,7 +11,18 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
+
+const { data: tracks } = useQuery({
+  queryKey: ['tracks'],
+  queryFn: fetchTracks
+});
+
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0)
+
+const goToNextTrack = () => {
+  setTrackIndex(trackIndex + 1);
+}
   return (
     <div className="App">
       <header className="App-header">
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,16 @@
-const apiToken: string = '';
+const apiToken = 'BQCnuAw9477-ipRjwhv2Krn6giPtwiDydEFLpAS7mw-UvBo4sqihQQqaVXyERaEdJGF1bT5MoKgbGACfL-g2QeLbcE1Uq6_Q-fCXDahVZRQcYXatcBLyJ95c0aQINpP98EU27dOh0gzpYi6OzlU7p0SNjHuh8tJptC-r7sBd3pnMQ-vDDg98Hl9TgaLvXohYvfQC9yfW4-PU3xMvdrwVYeqYWg';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+     throw new Error(`Fetching tracks failed with status ${response.status}`)
+   }
+  const data = await response.json() as { items: unknown[] };
+
+  return data.items;
 };
